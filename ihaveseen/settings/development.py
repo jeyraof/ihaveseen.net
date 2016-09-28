cf185755ff1d00f4a4c87c32a5eeee1d35888507
Jaeyoung Lee <jaeyoung@monodiary.net>
2016-09-28 19:04:38 +0900

    Start project

---
+++ ihaveseen/settings/development.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from .base import *
+
+DEBUG = True
+SECRET_KEY = 'development'
+ALLOWED_HOSTS += []
+
+DATABASE = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+    }
+}
+
---
+++ ihaveseen/settings/production.sample.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from .base import *
+
+DEBUG = False
+SECRET_KEY = 'production_secret_key'
+ALLOWED_HOSTS += []
+
+DATABASE = {
+    'default': {
+        'ENGINE': '',
+        'NAME': '',
+    }
+}
+
